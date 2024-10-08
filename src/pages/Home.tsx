2c463f4da2d95f97020c618f2a92b8e949f7ae5b
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-10-08 15:17:51 -0700

    font import fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ export default function Home() {
             <div className="flex flex-col justify-center items-center bg-orange-50 px-6 py-12 shadow sm:rounded-lg sm:px-12 rounded-lg">
                 <div className="text-center">
                     <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold tracking-wider text-gray-900 drop-shadow-md">Welcome</h1>
-                    <p className="text-4xl md:text-6xl lg:text-8xl text-gray-900 tracking-wider mt-2 mb-8 drop-shadow-md">Please Log In</p>
+                    <p className="text-xl md:text-2xl lg:text-4xl text-gray-900 tracking-wider mt-2 mb-8 drop-shadow-md">Please Log In</p>
                     <Link to='/login' className="action-button shadow">Login</Link>
                 </div>
             </div>
