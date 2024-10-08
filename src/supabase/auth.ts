a17a806b93ab3f73deff8136e35778b0a0d07c70
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-10-08 14:08:56 -0700

    env error fix

--- src/supabase/auth.ts
+++ src/supabase/auth.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseURL = import.meta.env.VITE_SUPA_URL
-const supabaseAnon = import.meta.env.VITE_SUPA_ANON
+const supabaseURL = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnon = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseURL, supabaseAnon);
